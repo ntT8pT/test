ba318fb5f5dd928ff12f0badb12e95600badf044
GitHub Action <action@github.com>
2024-04-19 00:35:24 +0000

    Initialize template repository proper
    Set package name to com.github.ntt8pt.test
    Set project name to test
    Set modid to test

--- src/main/java/com/github/ntt8pt/test/ExampleMod.java
+++ src/main/java/com/github/ntt8pt/test/ExampleMod.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.github.ntt8pt.test;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "test", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
--- src/main/java/com/github/ntt8pt/test/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/ntt8pt/test/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.ntt8pt.test.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
